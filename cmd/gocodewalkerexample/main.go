1b40e57ea61d925990454b6c8bc0614de301d14e
Ben Boyter <ben@boyter.org>
2023-03-22 17:15:45 +1100

    tweaks

--- cmd/gocodewalkerexample/main.go
+++ cmd/gocodewalkerexample/main.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	// useful for improving performance, then go tool pprof -http=localhost:8090 profile.pprof
+	//f, _ := os.Create("profile.pprof")
+	//pprof.StartCPUProfile(f)
+	//defer pprof.StopCPUProfile()
 
 	fileListQueue := make(chan *gocodewalker.File, 100)
 	fileWalker := gocodewalker.NewFileWalker(".", fileListQueue)
