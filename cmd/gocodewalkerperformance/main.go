684b81093f7731a01627dc23379c62acceb7e74d
Ben Boyter <ben@boyter.org>
2023-03-22 17:35:43 +1100

    addtional examples

--- cmd/gocodewalkerperformance/main.go
+++ cmd/gocodewalkerperformance/main.go
@@ -5,9 +5,16 @@ package main
 import (
 	"fmt"
 	"github.com/boyter/gocodewalker"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
+	// useful for improving performance, then go tool pprof -http=localhost:8090 profile.pprof
+	f, _ := os.Create("profile.pprof")
+	_ = pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
 	fileListQueue := make(chan *gocodewalker.File, 100)
 	fileWalker := gocodewalker.NewFileWalker(".", fileListQueue)
 
@@ -15,8 +22,7 @@ func main() {
 	// false which should cause continued processing to error
 	errorHandler := func(e error) bool {
 		fmt.Println("ERR", e.Error())
-		fileWalker.Terminate()
-		return false
+		return true
 	}
 	fileWalker.SetErrorHandler(errorHandler)
 
