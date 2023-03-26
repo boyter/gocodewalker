9068a79557fc98e64dd9741081d3137832c682ac
Ben Boyter <ben@boyter.org>
2023-03-27 09:00:22 +1100

    try out examples

--- cmd/gocodewalkerexample/main.go
+++ cmd/gocodewalkerexample/main.go
@@ -5,11 +5,16 @@ package main
 import (
 	"fmt"
 	"github.com/boyter/gocodewalker"
+	"regexp"
 )
 
 func main() {
 	fileListQueue := make(chan *gocodewalker.File, 100)
 	fileWalker := gocodewalker.NewFileWalker(".", fileListQueue)
+
+	fileWalker.AllowListExtensions = []string{"go", "sh"}
+	fileWalker.ExcludeListExtensions = []string{"sh"}
+	fileWalker.IncludeFilenameRegex = []*regexp.Regexp{regexp.MustCompile(".*")}
 
 	// handle the error by printing it out and terminating the walker and returning
 	// false which should cause continued processing to error
